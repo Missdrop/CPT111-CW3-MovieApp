77a2997c90e7191e2854cd331ca3f797be844bea
naipu <urarajemmy@gmail.com>
2025-11-07 21:26:22 +0800

    init basic&premium user

--- src/user/BasicUser.java
+++ src/user/BasicUser.java
@@ -1,5 +1,7 @@
 package user;
 
 public class BasicUser extends User {
-    
+    public BasicUser(String username, String password, java.util.List<String> watchlist, java.util.List<String> history) {
+        super(username, password, watchlist, history);
+    }
 }
--- src/user/PremiumUser.java
+++ src/user/PremiumUser.java
@@ -1,5 +1,7 @@
 package user;
 
 public class PremiumUser extends User {
-    
+    public PremiumUser(String username, String password, java.util.List<String> watchlist, java.util.List<String> history) {
+        super(username, password, watchlist, history);
+    }
 }
