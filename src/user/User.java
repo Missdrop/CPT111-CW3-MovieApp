3dea32613f53d02d83252a011fba0cd28b818575
naipu <urarajemmy@gmail.com>
2025-11-07 23:19:04 +0800

    edit User

--- src/user/User.java
+++ src/user/User.java
@@ -3,7 +3,6 @@ package user;
 import java.util.List;
 
 public class User {
-    //username,password,watchlist,history
     private String username;
     private String password;
     private List<String> watchlist;
