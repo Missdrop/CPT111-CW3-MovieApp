2bbb1ac124ce2ae498cc2637784417a6bac5c94c
naipu <urarajemmy@gmail.com>
2025-11-07 23:34:21 +0800

    make file manager protected

--- src/storage/FileManager.java
+++ src/storage/FileManager.java
@@ -11,7 +11,7 @@ import java.util.List;
 /**
  * This class is responsible for reading data from a csv file.
  */
-public class FileManager {
+protected class FileManager {
     private File file;
     private Scanner scanner;
 
