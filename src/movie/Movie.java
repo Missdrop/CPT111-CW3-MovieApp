2af061026b0be6bcbc9d129ea3f89a3b0fea5ca2
naipu <urarajemmy@gmail.com>
2025-11-07 19:59:02 +0800

    init Movie, User

--- src/movie/Movie.java
+++ src/movie/Movie.java
@@ -1,5 +1,9 @@
 package movie;
 
 public class Movie {
-    
+    private String id;
+    private String title;
+    private String genre;
+    private int year;
+    private double rating;
 }
--- src/user/User.java
+++ src/user/User.java
@@ -1,5 +1,11 @@
 package user;
 
+import java.util.List;
+
 public class User {
-    
+    //username,password,watchlist,history
+    private String username;
+    private String password;
+    private List<String> watchlist;
+    private List<String> history;
 }
