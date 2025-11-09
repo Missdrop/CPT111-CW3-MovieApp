fe8a9ce90bd151fc714ddb04bc273961964da948
naipu <urarajemmy@gmail.com>
2025-11-09 23:53:38 +0800

    add logout button

--- app/src/main/java/cpt111/group76/userinterface/Menu.java
+++ app/src/main/java/cpt111/group76/userinterface/Menu.java
@@ -1,11 +1,19 @@
 package cpt111.group76.userinterface;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import cpt111.group76.App;
+import cpt111.group76.storage.MovieManager;
 import cpt111.group76.user.User;
 
 public class Menu extends Application{
     private User user;
+    private static MovieManager movieManager = new MovieManager();
 
 
     public Menu(User user){
@@ -15,6 +23,20 @@ public class Menu extends Application{
 
     @Override
     public void start(javafx.stage.Stage primaryStage) {
+        Button logoutButton = new Button("Logout");
+        logoutButton.setOnAction(e -> {
+            movieManager.save();
+            movieManager.close();
+            new App().start(new Stage());
+            primaryStage.close();
+        });
+
+        VBox vbox = new VBox(10, logoutButton);
+        vbox.setPadding(new Insets(20));
+
+        Scene scene = new Scene(vbox, 300, 200);
+
+        primaryStage.setScene(scene);
         primaryStage.setTitle("Menu");
         primaryStage.show();
     }
