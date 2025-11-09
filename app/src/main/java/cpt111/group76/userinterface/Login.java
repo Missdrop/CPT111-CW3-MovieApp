fa855495f8c1ce4c470f0d4a35ce5bf5a420ff86
naipu <urarajemmy@gmail.com>
2025-11-09 19:38:37 +0800

    ignore resources

--- app/src/main/java/cpt111/group76/userinterface/Login.java
+++ app/src/main/java/cpt111/group76/userinterface/Login.java
@@ -1,5 +1,23 @@
 package cpt111.group76.userinterface;
 
-public class Login {
-    
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+// this class is a login gui with javafx, which has a username and password field and a login button
+public class Login extends Application{
+    @Override
+    public void start(javafx.stage.Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Login");
+        // Create a button for logging in
+        Button loginButton = new Button("Login");
+
+        // Create a scene and add the button to it
+        Scene scene = new Scene(loginButton, 300, 200);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+
 }
