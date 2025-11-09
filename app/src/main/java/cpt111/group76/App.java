d21f933b967be0ca1c89dbe362e2d5f9fa9864aa
naipu <urarajemmy@gmail.com>
2025-11-09 18:38:35 +0800

    update app.java

--- app/src/main/java/cpt111/group76/App.java
+++ app/src/main/java/cpt111/group76/App.java
@@ -1,7 +1,15 @@
 package cpt111.group76;
 
+import cpt111.group76.storage.MovieManager;
+import cpt111.group76.storage.UserManager;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        UserManager userManager = new UserManager();
+        MovieManager movieManager = new MovieManager();
+
+
+        userManager.close();
+        movieManager.close();
     }
 }
