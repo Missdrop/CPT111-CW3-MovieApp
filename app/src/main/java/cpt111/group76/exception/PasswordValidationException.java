a2479463c1f598fcf85097a9f3880f9f45d40bd9
naipu <urarajemmy@gmail.com>
2025-11-15 04:08:44 +0800

    add exception package

---
+++ app/src/main/java/cpt111/group76/exception/PasswordValidationException.java
@@ -0,0 +1,7 @@
+package cpt111.group76.exception;
+
+public class PasswordValidationException extends Exception {
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+}
---
+++ app/src/main/java/cpt111/group76/exception/UsernameValidationException.java
@@ -0,0 +1,7 @@
+package cpt111.group76.exception;
+
+public class UsernameValidationException extends Exception {
+    public UsernameValidationException(String message) {
+        super(message);
+    }
+}
